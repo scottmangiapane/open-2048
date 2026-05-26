0985bd07ed9b0b46ef2cdf52d84ab515c41e8c33
Scott Mangiapane <scottmangiapane@gmail.com>
2026-05-26 10:47:49 -0700

    Bumped version to 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.scottmangiapane.open2048"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
