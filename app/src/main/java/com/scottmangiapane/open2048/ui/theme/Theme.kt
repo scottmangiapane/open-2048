93dd6df96f269604de56fdeebd290c9d61812949
Scott Mangiapane <scottmangiapane@gmail.com>
2026-05-25 23:25:03 -0700

    Added more contrast to status bar in light mode

--- app/src/main/java/com/scottmangiapane/open2048/ui/theme/Theme.kt
+++ app/src/main/java/com/scottmangiapane/open2048/ui/theme/Theme.kt
@@ -9,8 +9,11 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Color(0xFF8F7A66),
@@ -51,6 +54,16 @@ fun Open2048Theme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            val windowInsetsController = WindowCompat.getInsetsController(window, view)
+            windowInsetsController.isAppearanceLightStatusBars = !darkTheme
+            windowInsetsController.isAppearanceLightNavigationBars = !darkTheme
+        }
     }
 
     MaterialTheme(
